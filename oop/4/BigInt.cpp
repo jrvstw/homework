a26175e4f44702db30495db7139f853344035356
Jarvis <ye.jia.wei@outlook.com>
2017-05-20 13:35:46 +0800

    .

--- oop/4/BigInt.cpp
+++ oop/4/BigInt.cpp
@@ -1,7 +1,11 @@
+// 402125039
+//dealing with the problem that I can change the value of addr[i]
+// deal with 0 in different digits
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <cstdlib>
-#define MAXDIGIT 5
+#define MAXDIGIT 3
 using namespace std;
 
 class BigInt
@@ -19,20 +23,13 @@ class BigInt
         friend ostream& operator <<(ostream& outputStream, const BigInt& A);
     private:
         int *addr;
-        int nInteger;
+        int nSegment;
 };
 
 int main()
 {
-    /*
-    BigInt a("23549");
-    cout << a << endl;
-    BigInt *b = new BigInt(3492);
-    cout << *b << endl;
-    BigInt c(a);
-    */
-    BigInt a("12321"), c(-a);
-    BigInt *b = new BigInt(11111);
+    BigInt a("111111111"), c(-a);
+    BigInt *b = new BigInt(110112112);
     c = a - *b;
     cout << a << endl;
     cout << *b << endl;
@@ -42,7 +39,7 @@ int main()
 
 BigInt::BigInt()
 {
-    nInteger = 1;
+    nSegment = 1;
     addr = new int(0);
 }
 
@@ -55,11 +52,11 @@ BigInt::BigInt(string A)
         A = A.substr(1, A.length() - 1);
     }
     //declaring dynamic array
-    nInteger = (A.length() - 1) / MAXDIGIT + 1;
-    addr = new int[nInteger];
+    nSegment = (A.length() - 1) / MAXDIGIT + 1;
+    addr = new int[nSegment];
     //assigning numbers
     int i = 0;
-    for (i = 0; i < nInteger - 1; i++) {
+    for (i = 0; i < nSegment - 1; i++) {
         addr[i] = atoi(A.substr(A.length() - MAXDIGIT, MAXDIGIT).c_str());
         if (isNegative)
             addr[i] = -addr[i];
@@ -73,82 +70,119 @@ BigInt::BigInt(string A)
 BigInt::BigInt(int A)
 {
     // setting divisor according to maxdigit
-    int i = MAXDIGIT;
     int divisor = 1;
-    while (i-- > 0)
+    for (int i = MAXDIGIT; i > 0; i--)
         divisor *= 10;
-    // assigning numbers
+    // counting nSegment and assigning numbers
     int tmp[10] = {0};
-    for (i = 0; A != 0; i++, A /= divisor)
-        tmp[i] = A % divisor;
-    nInteger = i;
-    addr = new int[nInteger];
-    for (i = 0; i < nInteger; i++)
+    for (nSegment = 0; A != 0; nSegment++, A /= divisor)
+        tmp[nSegment] = A % divisor;
+    addr = new int[nSegment];
+    for (int i = 0; i < nSegment; i++)
         addr[i] = tmp[i];
 }
 
 BigInt::BigInt(const BigInt &A)
 {
-    addr = new int[A.nInteger];
-    for (nInteger = 0; nInteger < A.nInteger; nInteger++)
-        addr[nInteger] = A.addr[nInteger];
+    addr = new int[A.nSegment];
+    for (nSegment = 0; nSegment < A.nSegment; nSegment++)
+        addr[nSegment] = A.addr[nSegment];
 }
 
 const BigInt BigInt::operator =(const BigInt B)
 {
     if (this != &B) {
         delete [] addr;
-        addr = new int[B.nInteger];
-        for (nInteger = 0; nInteger < B.nInteger; nInteger++)
-            addr[nInteger] = B.addr[nInteger];
+        addr = new int[B.nSegment];
+        for (nSegment = 0; nSegment < B.nSegment; nSegment++)
+            addr[nSegment] = B.addr[nSegment];
     }
     return *this;
 }
 
 const BigInt BigInt::operator +(const BigInt B) const
 {
-    // constructing a BigInt
+    // constructing a BigInt (overflow is considered)
     BigInt Result;
     delete [] Result.addr;
-    Result.nInteger = max(nInteger, B.nInteger);
-    Result.addr = new int[Result.nInteger];
+    Result.nSegment = max(nSegment, B.nSegment) + 1;
+    Result.addr = new int[Result.nSegment];
     // setting divisor according to maxdigit
     int divisor = 1;
     for (int i = MAXDIGIT; i > 0; i--)
         divisor *= 10;
-    // adding up
+    // adding up assuming the result is posotive
     int carry = 0;
-    for (int i = 0; i < Result.nInteger; i++) {
-        Result.addr[i] = addr[i] + B.addr[i] + carry;
+    for (int i = 0; i < Result.nSegment; i++) {
+        Result.addr[i] = carry;
+        if (i < nSegment)
+            Result.addr[i] += addr[i];
+        if (i < B.nSegment)
+            Result.addr[i] += B.addr[i];
+        // computing the next carry
+        carry = Result.addr[i] / divisor - (Result.addr[i] < 0);
+        Result.addr[i] -= carry * divisor;
+        /*
         carry = Result.addr[i] / divisor;
-        if (carry != 0)
-            Result.addr[i] %= divisor;
+        Result.addr[i] %= divisor;
+        if (Result.addr[i] < 0) {
+            carry--;
+            Result.addr[i] += divisor;
+        }
+        */
     }
-    // dealing with overflow
-    if (carry == 0)
+    // dealing with negative number
+    if (carry == -1) {
+        carry = 0;
+        for (int i = 0; i < Result.nSegment; i++) {
+            Result.addr[i] += carry;
+            carry = 0;
+            if (Result.addr[i] == 0)
+                continue;
+            Result.addr[i] -= divisor;
+            carry = 1;
+        }
+    }
+    // picking the proper number of segments
+    int properSegment = Result.nSegment;
+    while (Result.addr[properSegment - 1] == 0 && properSegment > 1)
+        properSegment--;
+    if (properSegment != Result.nSegment) {
+        int *tmp = new int[properSegment];
+        for (int i = 0; i < properSegment; i++)
+            tmp[i] = Result.addr[i];
+        delete [] Result.addr;
+        Result.addr = tmp;
+        Result.nSegment = properSegment;
+    }
+    return Result;
+    /*
+    if (carry > 0)
         return Result;
     else {
         BigInt Result2;
         delete [] Result2.addr;
-        Result2.nInteger = Result.nInteger + 1;
-        for (int i = 0; i < Result.nInteger; i++)
+        Result2.nSegment = Result.nSegment + 1;
+        for (int i = 0; i < Result.nSegment; i++)
             Result2.addr[i] = Result.addr[i];
-        Result2.addr[Result.nInteger] = carry;
+        Result2.addr[Result.nSegment] = carry;
         return Result2;
     }
+    */
 }
 
 const BigInt BigInt::operator -(const BigInt B) const
 {
     BigInt C(-B);
     BigInt Result = *this + C;
+    return Result;
 }
 
 const BigInt BigInt::operator -() const
 {
     BigInt A(*this);
     int i;
-    for (i = 0; i < nInteger; i++)
+    for (i = 0; i < nSegment; i++)
         A.addr[i] = -A.addr[i];
     return A;
 }
@@ -160,10 +194,9 @@ BigInt::~BigInt()
 
 ostream& operator <<(ostream& outputStream, const BigInt& A)
 {
-    outputStream << A.addr[A.nInteger - 1];
-    int i;
-    for (i = A.nInteger - 2; i >= 0; i--)
-        outputStream << abs(A.addr[i]);
+    outputStream << A.addr[A.nSegment - 1];
+    for (int i = A.nSegment - 2; i >= 0; i--)
+        outputStream << ',' << setw(MAXDIGIT) << setfill('0') << abs(A.addr[i]);
     return outputStream;
 }
 

210d5461cad8d84547b73ca3bf2496fdceb2ea91
Jarvis <jarvis@outlook.com>
2017-05-24 16:35:15 +0800

    .

--- sp/lab4/lab4/lab4.c
+++ sp/lab4/lab4/lab4.c
@@ -2,6 +2,7 @@
 #include "apue.h"
 
 void accumulation(int d_sum);
+void addfunc(int num, int *count);
 
 int main()
 {
@@ -17,11 +18,28 @@ int main()
 	/**********************************************************/
 
 	//Implement your code.
+    TELL_WAIT();
+    int count = 0;
+    for (int i = 0; i < 5; i++) {
+        if ((pid[i] = fork()) < 0)
+            err_sys("fork error");
+        else if (pid[i] == 0)
+        {
+            WAIT_PARENT();
+            addfunc(i, &count);
+            TELL_PARENT(getppid());
+            exit(0);
+        }
+        else
+            TELL_CHILD(pid[i]);
+            WAIT_CHILD();
+    }
 
 	/**********************************************************/
 
 	sum = fopen("sum.txt", "r");
 	fscanf(sum, "%d", &total_sum);
+    //printf("count = %d\n", count);
 	printf("Day_Average = %d\n",total_sum/(year*week*day));
 	fclose(sum);
 
@@ -44,3 +62,26 @@ void accumulation(int d_sum)    //Accumulating the daily sum to "sum.txt".
 
 	return;
 }
+
+void addfunc(int num, int *count)
+{
+    FILE *current;
+    int ptr = 0;
+    char fname[] = "0-00.txt";
+
+    fname[0] = '1' + num;
+    for (int k = 1; k <= 52; k++) {
+        fname[2] = '0' + k / 10;
+        fname[3] = '0' + k % 10;
+        current = fopen(fname, "r");
+        for (int j = 0; j < 7; j++)
+            for (int i = 0; i < 96; i++) {
+                fscanf(current, "%d", &ptr);
+                accumulation(ptr);
+                (*count)++;
+            }
+        fclose(current);
+    }
+    return;
+}
+

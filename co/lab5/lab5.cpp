2b16f3857fd386fa661f11132619bc4ae0e56e25
Ye Jia-wei <ye.jia.wei@outlook.com>
2018-01-06 21:57:59 +0800

    autosave

--- co/lab5/lab5.cpp
+++ co/lab5/lab5.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     // check validity of input
+
     // freopen
+    freopen("gcc100.din", "r", stdin);
+
     // calculate output
+    int label;
+    string addr;
+    while (cin >> label >> addr) {
+        cout << label << " " << addr << endl;
+    }
 
     return 0;
 }

caa0f58e3b4d95f9baddf602b0fc54a7dea23c55
Ye Jia-wei <ye.jia.wei@outlook.com>
2018-01-07 01:16:36 +0800

    autosave

--- co/lab5/lab5.cpp
+++ co/lab5/lab5.cpp
@@ -2,18 +2,36 @@
 #include <string>
 using namespace std;
 
+class block
+{
+    public:
+    private:
+        int valid;
+        int dirty;
+        int tag;
+        int data;
+};
+
 int main(int argc, char *argv[])
 {
     // check validity of input
+    int     cache_size  = atoi(argv[1]) << 8; // in terms of words
+    int     block_size  = atoi(argv[2]) >> 2; // in terms of words
+    int     assoc       = (argv[3]=="f")? 0: atoi(argv[3]);
+    string  r_policy    = argv[4];
+
+    cout << cache_size << " " << block_size << " " << assoc << " " << r_policy << endl;
 
     // freopen
-    freopen("gcc100.din", "r", stdin);
+    freopen(argv[5], "r", stdin);
+
+    // declare a cache
 
     // calculate output
     int label;
-    string addr;
-    while (cin >> label >> addr) {
-        cout << label << " " << addr << endl;
+    int addr;
+    while (cin >> label >> hex >> addr) {
+        cout << label << " " << hex << addr << endl;
     }
 
     return 0;

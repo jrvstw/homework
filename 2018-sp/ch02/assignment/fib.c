2133b78076879b7fffbd474224ce9900efb606fc
Ye Jia-wei <ye.jia.wei@outlook.com>
2018-03-21 13:42:07 +0800

    autosave

--- 2018-sp/ch02/assignment/fib.c
+++ 2018-sp/ch02/assignment/fib.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
+int fib(int n)
+{
+    int a = 0,
+        b = 1;
+    for (int i = 0; i < n/2; i++) {
+        a += b;
+        b += a;
+    }
+}
+
 int main()
 {
     int input;
-    scanf("%d", input);
-    printf("%d\n", input);
+    while (scanf("%d", &input)) {
+        printf("%d\n", fib(input));
+    }
     return 0;
 }
 
